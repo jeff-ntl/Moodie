5e8d5510a549eb06b683eaedc03ff90b94bdbc4b
jeff-ntl <jeff2406n@gmail.com>
2019-03-11 13:17:43 +0000

    add listener on diary card

--- app/src/main/java/android/example/com/moodie/activities/DiaryAdapter.kt
+++ app/src/main/java/android/example/com/moodie/activities/DiaryAdapter.kt
@@ -8,8 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.card_diary.view.*
 
+//represent click events on the diary card
+interface DiaryListener {
+    fun onDiaryClick(diary: DiaryModel)
+}
+
 //?takes in a list, return recycleview adapter (to hold the data in the list)
-class DiaryAdapter constructor(private var diaries: List<DiaryModel>) : RecyclerView.Adapter<DiaryAdapter.MainHolder>() {
+class DiaryAdapter constructor(private var diaries: List<DiaryModel>,  private val listener: DiaryListener) : RecyclerView.Adapter<DiaryAdapter.MainHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
         return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_diary, parent, false))
@@ -17,16 +22,17 @@ class DiaryAdapter constructor(private var diaries: List<DiaryModel>) : Recycler
 
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
         val diary = diaries[holder.adapterPosition]
-        holder.bind(diary)
+        holder.bind(diary, listener)
     }
 
     override fun getItemCount(): Int = diaries.size
 
     class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(diary: DiaryModel) {
+        fun bind(diary: DiaryModel, listener: DiaryListener) {
             itemView.diaryTitle.text = diary.title
             itemView.diaryDescription.text = diary.description
+            itemView.setOnClickListener { listener.onDiaryClick(diary) }
         }
     }
 }
